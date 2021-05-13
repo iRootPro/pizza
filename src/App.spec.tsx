8ac57e8b2f2b910f3e475a8835fce3640c2d6484
Alexander Neupokoev <admin@armavir.ru>
2021-05-13 12:54:10 +0300

    testing routing

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -4,56 +4,68 @@ import {createMemoryHistory} from "history";
 import React from "react";
 import App from "./App";
 
+const renderWithRouter = (component: JSX.Element | JSX.Element[], initRoute: string = '/') => {
+    const history = createMemoryHistory({initialEntries: [initRoute]})
+    return {
+        ...render(
+            <Router history={history}>
+                {component}
+            </Router>
+        )
+    }
+}
+
 
 describe('App', () => {
+
+    describe('Home link click', () => {
+        it('navigates to home page', () => {
+            const {container, getByText} = renderWithRouter(<App/>, '/order')
+            expect(container.innerHTML).toMatch('Order Page')
+            fireEvent.click(getByText(/home/i))
+            expect(container.innerHTML).toMatch('Pizza Constructor')
+        })
+    })
+
     describe('Registration link click', () => {
         it('navigates to registration page', () => {
-            const history = createMemoryHistory()
-            const {container, getByText} = render(
-                <Router history={history}>
-                    <App/>
-                </Router>
-            )
-
-            expect(container.innerHTML).toMatch('Big')
-            expect(container.innerHTML).toMatch('Registration')
-            fireEvent.click(getByText("Registration"))
+            const {container, getByText} = renderWithRouter(<App/>)
+            expect(container.innerHTML).toMatch('Pizza Constructor')
+            fireEvent.click(getByText(/registration/i))
             expect(container.innerHTML).toMatch('Registration Page')
         })
     })
 
     describe('Login link click', () => {
         it('navigates to login page', () => {
-            const history = createMemoryHistory()
-            const {container, getByText} = render(
-                <Router history={history}>
-                    <App/>
-                </Router>
-            )
-
-            expect(container.innerHTML).toMatch('big')
+            const {container, getByText} = renderWithRouter(<App/>)
+            expect(container.innerHTML).toMatch('Pizza Constructor')
             fireEvent.click(getByText(/login/i))
             expect(container.innerHTML).toMatch('Login Page')
         })
     })
 
     describe('Order link click', () => {
-        it.todo('navigates to order page')
+        it('navigates to order page', () => {
+            const {container, getByText} = renderWithRouter(<App/>)
+            expect(container.innerHTML).toMatch('Pizza Constructor')
+            fireEvent.click(getByText(/order/i))
+            expect(container.innerHTML).toMatch('Order Page')
+        })
     })
 
     describe('Receipt link click', () => {
-        it.todo('navigate to receipt page')
+        it('navigate to receipt page', () => {
+            const {container, getByText} = renderWithRouter(<App/>)
+            expect(container.innerHTML).toMatch('Pizza Constructor')
+            fireEvent.click(getByText(/receipt/i))
+            expect(container.innerHTML).toMatch('Receipt Page')
+        })
     })
 
     describe('With an unsupported URL', () => {
         it('shows 404 page', () => {
-            const history = createMemoryHistory()
-            const {container, getByText} = render(
-                <Router history={history}>
-                    <App/>
-                </Router>
-            )
-            history.push('/some/bad/route')
+            const {container} = renderWithRouter(<App/>, '/some/bad/route')
             expect(container.innerHTML).toMatch('404 Page Not Found')
         })
     })
